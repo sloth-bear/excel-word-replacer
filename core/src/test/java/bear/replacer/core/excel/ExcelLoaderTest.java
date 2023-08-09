48f0603014603731e14c6e2b6b2f49af064c8dd6
sloth-bear <slothbear.hj@gmail.com>
2023-08-09 17:12:47 +0900

    Fix failed test cases cause of file path

--- core/src/test/java/bear/replacer/core/excel/ExcelLoaderTest.java
+++ core/src/test/java/bear/replacer/core/excel/ExcelLoaderTest.java
@@ -4,14 +4,18 @@ import bear.replacer.core.excel.model.ExcelLoadFailedException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 class ExcelLoaderTest {
 
     @Test
-    void givenExcelFile_whenLoad_thenReturnXSSFWorkbook() {
+    void givenExcelFile_whenLoad_thenReturnXSSFWorkbook() throws IOException {
         // given
-        final String filePath = "C:\\Users\\admin\\Documents\\git-repo\\excel-word-replacer\\src\\test\\resources\\input\\test_excel.xlsx";
-        final ExcelLoader excelLoader = new ExcelLoader(filePath);
+        final ClassPathResource resource = new ClassPathResource("input/test_excel.xlsx");
+        final ExcelLoader excelLoader = new ExcelLoader(Paths.get(resource.getURI()).toString());
 
         // when
         final Workbook workbook = excelLoader.load();
@@ -22,10 +26,10 @@ class ExcelLoaderTest {
     }
 
     @Test
-    void givenNonExcelFile_whenLoad_thenThrownExcelLoadFailedException() {
+    void givenNonExcelFile_whenLoad_thenThrownExcelLoadFailedException() throws IOException {
         // given
-        final String csvFilePath = "C:\\Users\\admin\\Documents\\git-repo\\excel-word-replacer\\src\\test\\resources\\rule.csv";
-        final ExcelLoader excelLoader = new ExcelLoader(csvFilePath);
+        final ClassPathResource resource = new ClassPathResource("rule.csv");
+        final ExcelLoader excelLoader = new ExcelLoader(Paths.get(resource.getURI()).toString());
 
         // when && then
         Assertions.assertThatThrownBy(excelLoader::load).isInstanceOf(ExcelLoadFailedException.class);
--- core/src/test/java/bear/replacer/core/excel/ExcelReplacerTest.java
+++ core/src/test/java/bear/replacer/core/excel/ExcelReplacerTest.java
@@ -5,16 +5,21 @@ import bear.replacer.core.rule.RuleLoader;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 class ExcelReplacerTest {
 
-    private static final String EXCEL_FILE_PATH = "C:\\Users\\admin\\Documents\\git-repo\\excel-word-replacer\\src\\test\\resources\\input\\test_excel.xlsx";
+    private static final String EXCEL_FILE_PATH = "input\\test_excel.xlsx";
     private static final String RULE_FILE_PATH = "rule.csv";
 
     @Test
-    void givenWorkbook_whenReplace_thenReturnReplacedWorkbook() {
+    void givenWorkbook_whenReplace_thenReturnReplacedWorkbook() throws IOException {
         // given
-        final ExcelLoader excelLoader = new ExcelLoader(EXCEL_FILE_PATH);
+        final ClassPathResource resource = new ClassPathResource(EXCEL_FILE_PATH);
+        final ExcelLoader excelLoader = new ExcelLoader(Paths.get(resource.getURI()).toString());
         final Workbook source = excelLoader.load();
 
         final RuleLoader ruleLoader = new CsvRuleLoader(ClassLoader.getSystemResourceAsStream(RULE_FILE_PATH));
